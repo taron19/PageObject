2d692415cb82d3f31eb40460fc9649b22ddb47ef
Taron Gukasyan <funkadelica@mail.ru>
2025-10-14 21:02:26 +0300

    added new branch + java fakers

--- src/test/java/logic/demoqa/form/TextBoxTests.java
+++ src/test/java/logic/demoqa/form/TextBoxTests.java
@@ -1,6 +1,7 @@
 package logic.demoqa.form;
 
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/logic/demoqa/registration/BaseTestImplRegistration.java
+++ src/test/java/logic/demoqa/registration/BaseTestImplRegistration.java
@@ -1,56 +1,60 @@
 package logic.demoqa.registration;
 
+
 import org.junit.jupiter.api.Test;
+import utils.FakerUtils;
 
 public class BaseTestImplRegistration extends BaseTest {
 
     private final RegistrationPage registrationPage = new RegistrationPage();
+    private final FakerUtils fakerUtils = new FakerUtils();
+
 
 
     @Test
     void successfulRegistration(){
         registrationPage.openPage()
-                .setFirstName("Igor")
-                .setLastName("Igorov")
-                .setUserEmail("i@mail.ru")
-                .setGender("Male")
-                .setUserNumber("9096776688")
-                .setDateOfBirthInput("2023","December","22")
-                .setSubject("Maths")
-                .setHobbies("Reading")
-                .setPicture("Снимок экрана 2025-02-21 130657.png")
-                .setAddress("Flot st")
-                .setState("Haryana")
-                .setCity("Panipat");
+                .setFirstName(fakerUtils.getFirstName())
+                .setLastName(fakerUtils.getLastName())
+                .setUserEmail(fakerUtils.getEmail())
+                .setGender(fakerUtils.getGender())
+                .setUserNumber(fakerUtils.getPhoneNumber())
+                .setDateOfBirthInput(fakerUtils.getYear(), fakerUtils.getMonth(), fakerUtils.getDay())
+                .setSubject(fakerUtils.getSubject())
+                .setHobbies(fakerUtils.getHobby())
+                .setPicture(fakerUtils.getPictureURL())
+                .setAddress(fakerUtils.getAdress())
+                .setState(fakerUtils.selectState())
+                .setCity(fakerUtils.selectCity());
 
 
-        registrationPage.getCheckingComponent().checkFirstNameAndLastName("Igor Igorov");
-        registrationPage.getCheckingComponent().checkEmail("i@mail.ru");
-        registrationPage.getCheckingComponent().checkGender("Male");
-        registrationPage.getCheckingComponent().checkNumber("9096776688");
-        registrationPage.getCheckingComponent().checkDate("22 December,2023");
-        registrationPage.getCheckingComponent().checkSubject("Maths");
-        registrationPage.getCheckingComponent().checkHobby("Reading");
-        registrationPage.getCheckingComponent().checkPictureData("Снимок экрана 2025-02-21 130657.png");
-        registrationPage.getCheckingComponent().checkAddress("Flot st");
-        registrationPage.getCheckingComponent().checkStateAndCity("Haryana Panipat");
+        registrationPage.getCheckingComponent().checkFirstNameAndLastName(fakerUtils.getFirstName()+" "+fakerUtils.getLastName());
+        registrationPage.getCheckingComponent().checkEmail(fakerUtils.getEmail());
+        registrationPage.getCheckingComponent().checkGender(fakerUtils.getGender());
+        registrationPage.getCheckingComponent().checkNumber(fakerUtils.getPhoneNumber());
+        registrationPage.getCheckingComponent().checkDate(fakerUtils.getDay()+" "+fakerUtils.getMonth()+","+fakerUtils.getYear());
+        registrationPage.getCheckingComponent().checkSubject(fakerUtils.getSubject());
+        registrationPage.getCheckingComponent().checkHobby(fakerUtils.getSubject());
+        registrationPage.getCheckingComponent().checkPictureData(fakerUtils.getSubject());
+        registrationPage.getCheckingComponent().checkAddress(fakerUtils.getAdress());
+        registrationPage.getCheckingComponent().checkStateAndCity(fakerUtils.selectState()+" "+fakerUtils.selectCity());
     }
 
     @Test
     void unsuccessfulRegistration_ShouldNotContainWrongData() {
         registrationPage.openPage()
-                .setFirstName("Igor")
-                .setLastName("Igorov")
-                .setUserEmail("i@mail.ru")
-                .setGender("Male")
-                .setUserNumber("9096776688")
-                .setDateOfBirthInput("2023","December","22")
-                .setSubject("Maths")
-                .setHobbies("Reading")
-                .setPicture("Снимок экрана 2025-02-21 130657.png")
-                .setAddress("Flot st")
-                .setState("Haryana")
-                .setCity("Panipat");
+                .setFirstName(fakerUtils.getFirstName())
+                .setLastName(fakerUtils.getLastName())
+                .setUserEmail(fakerUtils.getEmail())
+                .setGender(fakerUtils.getGender())
+                .setUserNumber(fakerUtils.getPhoneNumber())
+                .setDateOfBirthInput(fakerUtils.getYear(), fakerUtils.getMonth(), fakerUtils.getDay())
+                .setSubject(fakerUtils.getSubject())
+                .setHobbies(fakerUtils.getHobby())
+                .setPicture(fakerUtils.getPictureURL())
+                .setAddress(fakerUtils.getAdress())
+                .setState(fakerUtils.selectState())
+                .setCity(fakerUtils.selectCity());
 
 
         registrationPage.getCheckingComponent().checkFirstNameAndLastNameNegative("Wrong Name");
