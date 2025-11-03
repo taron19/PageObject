5a2f8ab7b4979d5c87e0bbc16611590981e29781
Taron Gukasyan <funkadelica@mail.ru>
2025-11-03 14:59:03 +0300

    added tasks

--- src/test/java/logic/demoqa/registration/BaseTestImplRegistration.java
+++ src/test/java/logic/demoqa/registration/BaseTestImplRegistration.java
@@ -1,6 +1,7 @@
 package logic.demoqa.registration;
 
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -14,6 +15,7 @@ public class BaseTestImplRegistration extends BaseTest {
 
 
     @Test
+    @Tag("success registration")
     void successfulRegistration() {
 
 
