c484dcebacbce9c538651aaa636300c50581ba62
Taron Gukasyan <funkadelica@mail.ru>
2025-11-05 14:04:55 +0300

    added property data

--- src/test/java/logic/demoqa/registration/BaseTest.java
+++ src/test/java/logic/demoqa/registration/BaseTest.java
@@ -29,7 +29,7 @@ public class BaseTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.timeout=6000;
         Configuration.remote = System.getProperty("remoteUrl");
-        Configuration.browser = System.getProperty("browser");
+        Configuration.browser = System.getProperty("browser","chrome");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
