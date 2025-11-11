30a297a708e5d249526758c58bdcbc404cc19c58
Taron Gukasyan <funkadelica@mail.ru>
2025-11-11 15:38:37 +0300

    corrections

--- src/test/java/logic/demoqa/registration/BaseTest.java
+++ src/test/java/logic/demoqa/registration/BaseTest.java
@@ -31,7 +31,7 @@ public class BaseTest {
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.timeout = 6000;
-        Configuration.remote = System.getProperty("remote");
+        Configuration.remote = System.getProperty("remote","https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.browser = System.getProperty("browserConfig", "chrome");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
